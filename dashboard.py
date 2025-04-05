391af97bf50a84c5786d3d621271f3b42d3d0780
JesusPaucarE <151387205+JesusPaucarE@users.noreply.github.com>
2025-04-05 18:35:36 -0300

    Add files via upload
    Primera actualización de mi primer programa

---
+++ dashboard.py
@@ -0,0 +1,24 @@
+
+
+############## Desarrolle su codigo a continuacion ###################
+
+import streamlit as st
+
+st.title("Hola Mundo")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################################################################
